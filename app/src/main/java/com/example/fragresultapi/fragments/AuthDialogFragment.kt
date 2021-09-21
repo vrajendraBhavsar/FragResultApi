bd1c6c4c8b8df4370649eb67c48d93c85a2ddd1e
Vrajendra <vraj.bhavsar.mi@gmail.com>
2021-09-21 09:44:59 +0530

    - Changed button name, and added its updating name when pressed. - AuthDialogFragment => added button click operation.

--- app/src/main/java/com/example/fragresultapi/fragments/AuthDialogFragment.kt
+++ app/src/main/java/com/example/fragresultapi/fragments/AuthDialogFragment.kt
@@ -6,11 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.view.postDelayed
 import com.example.fragresultapi.R
+import com.example.fragresultapi.databinding.FragmentAuthDialogBinding
+
 class AuthDialogFragment : AppCompatDialogFragment() {
+    lateinit var binding: FragmentAuthDialogBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAuthDialogBinding.bind(view)    //fragment ViewBinding
+
         // First, get the resultKey from the caller.
         val requestKey = arguments?.getString(EXTRA_RESULT_KEY, null)
         if (requestKey.isNullOrEmpty()){
@@ -18,17 +24,27 @@ class AuthDialogFragment : AppCompatDialogFragment() {
             return
         }
 
+        // Next, once user clicks "Auth", access the auth service and get the result.
+        binding.btnAuth.setOnClickListener {
+            val userName = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
 
+            if (userName == password) {
+                binding.btnAuth.text = resources.getText(R.string.verifying)
+                view.postDelayed(1500){
+                    childFragmentManager.setFragmentResult(requestKey, Bundle().apply {
+                        putString(RESULT_AUTH_USER, userName)
+                    })
+                    dismiss()
+                }
+            }
+        }
     }
 
 //    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 //        // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_auth_dialog, container, false)
 //    }
-
-    // Next, once user clicks "Auth", access the auth service and get the result.
-
-
     companion object{
         const val EXTRA_RESULT_KEY = "extra_result_key"
         const val RESULT_AUTH_USER = "result_auth_user"
