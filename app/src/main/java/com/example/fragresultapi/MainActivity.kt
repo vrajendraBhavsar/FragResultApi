35ad62ad2c0d0c8de16e913f5fb8dd56017430ed
Vrajendra <vraj.bhavsar.mi@gmail.com>
2021-09-21 17:47:09 +0530

    - created UI for MasterFragment and add logics in its fragment.

--- app/src/main/java/com/example/fragresultapi/MainActivity.kt
+++ app/src/main/java/com/example/fragresultapi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.fragresultapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragresultapi.databinding.ActivityMainBinding
+import com.example.fragresultapi.fragments.MasterFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(MasterFragment::class.java, "")
     }
 }
--- app/src/main/java/com/example/fragresultapi/fragments/MasterFragment.kt
+++ app/src/main/java/com/example/fragresultapi/fragments/MasterFragment.kt
@@ -2,44 +2,45 @@ package com.example.fragresultapi.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.system.Os.remove
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.example.fragresultapi.R
 import com.example.fragresultapi.databinding.FragmentMasterBinding
 import java.util.*
 
 class MasterFragment : Fragment() {
     lateinit var binding: FragmentMasterBinding
+
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMasterBinding.bind(view)
 
         childFragmentManager.setFragmentResultListener(request_key, this) { _, bundle ->
-            val userNameFromDialog = bundle.getString(AuthDialogFragment.RESULT_AUTH_USER, "Unknown")
+            val userNameFromDialog =
+                bundle.getString(AuthDialogFragment.RESULT_AUTH_USER, "Unknown")
             binding.tvMaster.text = "Auth user: $userNameFromDialog"
             Toast.makeText(requireContext(), "Auth successfully!", Toast.LENGTH_LONG).show()
             childFragmentManager.clearFragmentResultListener(request_key)   //cleared listener
         }
 
         binding.btnMaster.setOnClickListener {
-            childFragmentManager.openAuthDialog()
+            childFragmentManager.openAuthDialog(request_key)
         }
     }
 
-    companion object{
+    companion object {
         val request_key = UUID.randomUUID().toString()
     }
 
     fun FragmentManager.openAuthDialog(resultKey: String) {
         val existing = findFragmentByTag("AuthDialogFragment")
-        if (existing != null){
-            remove(existing)
+        if (existing != null) {
+            childFragmentManager
+                .beginTransaction()
+                .remove(existing)
+                .commit()
         }
         val newDialog = AuthDialogFragment().apply {
             val bundle = Bundle()
